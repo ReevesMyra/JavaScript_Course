e5bd0baaea69df7384c07f21488fb5dd970a80f9
ReevesMyra <154421142+ReevesMyra@users.noreply.github.com>
2024-05-07 21:59:26 -0700

    Step 54

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -10,4 +10,8 @@ window.alert(ryanGeorge);
 
 document.write('<br> Lisa told Bart, "Knock it off, Bart! Or I\'ll tell dad!" <br> "Eat my shorts" Bart replied!');
 
-document.write('<br> "Be who you are and say what you feel,' + ' because those who mind don\'t matter and those who matter don\'t mind."' + ' - Dr. Seuss')
+document.write('<br> "Be who you are and say what you feel,' + ' because those who mind don\'t matter and those who matter don\'t mind."' + ' - Dr. Seuss <br>');
+
+let B = 'This ' + 'is ' + 'a' + ' concatenated' + ' string';
+document.write(B);
+
