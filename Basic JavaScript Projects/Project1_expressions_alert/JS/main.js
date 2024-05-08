98b07abeef2c4a2e8018238b91e23df7291c4ba0
ReevesMyra <154421142+ReevesMyra@users.noreply.github.com>
2024-05-07 21:56:00 -0700

    Fulfills Step 51

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -7,3 +7,5 @@ document.write(A);
 
 let ryanGeorge = 'Hi there, hello!';
 window.alert(ryanGeorge);
+
+document.write('<br> Lisa told Bart, "Knock it off, Bart! Or I\'ll tell dad!" <br> "Eat my shorts" Bart replied!');
