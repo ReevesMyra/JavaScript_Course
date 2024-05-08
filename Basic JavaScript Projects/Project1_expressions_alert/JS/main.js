602c50a4e0f7fc144c43bb908c6fbc881c34e4d8
ReevesMyra <154421142+ReevesMyra@users.noreply.github.com>
2024-05-07 22:01:39 -0700

    Step 56

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,17 +1,35 @@
 window.alert("Hello, world!");
 
+
+
 document.write('Hello, world  ...written on the page!');
+
+
 
 var A = '<br> This is a string';
 document.write(A);
 
+
+
 let ryanGeorge = 'Hi there, hello!';
 window.alert(ryanGeorge);
 
+
+
 document.write('<br> Lisa told Bart, "Knock it off, Bart! Or I\'ll tell dad!" <br> "Eat my shorts" Bart replied!');
+
+
 
 document.write('<br> "Be who you are and say what you feel,' + ' because those who mind don\'t matter and those who matter don\'t mind."' + ' - Dr. Seuss <br>');
 
-let B = 'This ' + 'is ' + 'a' + ' concatenated' + ' string';
+
+
+let B = 'This ' + 'is ' + 'a' + ' concatenated' + ' string, ';
 document.write(B);
+
+
+
+const Characters = 'The Heeler Family', Dad = 'Bandit', Mum = 'Chilli', Eldest = 'Bluey', Youngest = 'Bingo', Uncle1 = 'Stripe', Cousin1 = 'Muffin', Cousin2 = 'Socks', Aunt1 = 'Trixie', Uncle2 = 'Radley', Aunt2 = 'Frisky';
+document.write(Eldest);
+
 
